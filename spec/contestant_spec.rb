2dfc3be8b040e6fb422ecc3553546c26fcf451f6
KaraJH <karas.jones@yahoo.com>
2022-12-21 10:57:30 -0700

    test: Out of state method

--- spec/contestant_spec.rb
+++ spec/contestant_spec.rb
@@ -10,8 +10,19 @@ RSpec.describe Contestant do
     expect(alexander).to be_a(Contestant)
     expect(alexander.state_of_residence).to eq("CO")
     expect(alexander.spending_money).to eq(10)
-    expect(alexander.out_of_state?).to eq(false)
     expect(alexander.game_interests).to eq([])
     expect(alexander.full_name).to eq("Alexander Aigiades")
+  end
+
+  describe '#out_of_state?' do 
+    it 'returns true if they are in Colorado' do 
+      alexander = Contestant.new({first_name: 'Alexander',
+                                      last_name: 'Aigiades',
+                                      age: 28,
+                                      state_of_residence: 'CO',
+                                      spending_money: 10})
+    
+       expect(alexander.out_of_state?).to eq(false)                              
+    end
   end
 end
