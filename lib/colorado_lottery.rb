2c70d80edc283bce3133d8bb6cc78de10db004a5
KaraJH <karas.jones@yahoo.com>
2022-12-21 12:10:39 -0700

    feat: Registered contestans hash

--- lib/colorado_lottery.rb
+++ lib/colorado_lottery.rb
@@ -1,5 +1,5 @@
 class ColoradoLottery 
-  attr_reader :registered_contestants, :winners, :current_contestants
+  attr_reader :registered_contestants, :winners, :current_contestants, :collect_contestants
 
   def initialize
     @registered_contestants = {}
@@ -14,4 +14,17 @@ class ColoradoLottery
   def can_register?(contestant, game)
     (interested_and_18?(contestant, game)) && (contestant.out_of_state? == false || game.national_drawing? == true)
   end
+
+  def register_contestant(contestant, game)
+    if registered_contestants[game.name] == nil 
+      registered_contestants[game.name] = []
+    end
+      @registered_contestants[game.name] << contestant
+      
+    @registered_contestants
+  end
+
+
+
+
 end
