7a533dab4db9a89150cd9f1fb38440dad0899690
KaraJH <karas.jones@yahoo.com>
2022-12-21 11:02:17 -0700

    test: Add game interest method

--- spec/contestant_spec.rb
+++ spec/contestant_spec.rb
@@ -1,4 +1,5 @@
 require './lib/contestant'
+require './lib/game'
 
 RSpec.describe Contestant do 
   it 'exists and has attributes' do 
@@ -23,6 +24,27 @@ RSpec.describe Contestant do
                                       spending_money: 10})
     
        expect(alexander.out_of_state?).to eq(false)                              
+    end
+  end
+
+  describe '#add_game_interest' do 
+    it 'can add games to their game interests' do 
+      alexander = Contestant.new({first_name: 'Alexander',
+                                      last_name: 'Aigiades',
+                                      age: 28,
+                                      state_of_residence: 'CO',
+                                      spending_money: 10})
+      
+      pick_4 = Game.new('Pick 4', 2)
+      mega_millions = Game.new('Mega Millions', 5, true)
+
+      expect(alexander.game_interests).to eq([])
+
+      alexander.add_game_interest('Mega Millions')
+      alexander.add_game_interest('Pick 4')
+
+      expect(alexander.game_interests).to eq(["Mega Millions", "Pick 4"])
+      
     end
   end
 end
