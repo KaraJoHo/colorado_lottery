2dfaea4f66b0fab6fad6f9e67e3d555f1b450e13
KaraJH <karas.jones@yahoo.com>
2022-12-21 12:23:44 -0700

    test: Eligible contestants method

--- spec/colorado_lottery_spec.rb
+++ spec/colorado_lottery_spec.rb
@@ -164,6 +164,83 @@ RSpec.describe ColoradoLottery do
                     'Mega Millions' => [alexander, frederick, winston, grace],
                     'Cash 5' => [winston, grace]
       }
+
+      expect(lottery.registered_contestants).to eq(expected2)
+    end
+  end
+
+  describe '#eligible_contestants' do 
+    it 'returns a list of eligible contestants for the given game who have more spending money than the cost' do 
+      lottery = ColoradoLottery.new 
+      pick_4 = Game.new('Pick 4', 2)
+
+      mega_millions = Game.new('Mega Millions', 5, true)
+      cash_5 = Game.new('Cash 5', 1)
+
+      alexander = Contestant.new({
+                            first_name: 'Alexander',
+                            last_name: 'Aigades',
+                            age: 28,
+                            state_of_residence: 'CO',
+                            spending_money: 10})
+      
+      benjamin = Contestant.new({
+                            first_name: 'Benjamin',
+                            last_name: 'Franklin',
+                            age: 17,
+                            state_of_residence: 'PA',
+                            spending_money: 100})
+      
+      frederick = Contestant.new({
+                            first_name:  'Frederick',
+                            last_name: 'Douglass',
+                            age: 55,
+                            state_of_residence: 'NY',
+                            spending_money: 20})
+
+      winston = Contestant.new({
+                          first_name: 'Winston',
+                          last_name: 'Churchill',
+                          age: 18,
+                          state_of_residence: 'CO',
+                          spending_money: 5})
+
+      alexander.add_game_interest('Pick 4')
+      alexander.add_game_interest('Mega Millions')
+
+      frederick.add_game_interest('Mega Millions')
+
+      winston.add_game_interest('Cash 5')
+      winston.add_game_interest('Mega Millions')
+
+      benjamin.add_game_interest('Mega Millions')
+
+      lottery.register_contestant(alexander, pick_4) 
+      lottery.register_contestant(alexander, mega_millions) 
+
+      lottery.register_contestant(frederick, mega_millions)
+
+      lottery.register_contestant(winston, cash_5)
+      lottery.register_contestant(winston, mega_millions)
+
+      grace = Contestant.new({
+                     first_name: 'Grace',
+                     last_name: 'Hopper',
+                     age: 20,
+                     state_of_residence: 'CO',
+                     spending_money: 20})
+
+
+      grace.add_game_interest('Mega Millions')
+      grace.add_game_interest('Cash 5')
+      grace.add_game_interest('Pick 4')
+      lottery.register_contestant(grace, mega_millions)
+      lottery.register_contestant(grace, cash_5)
+      lottery.register_contestant(grace, pick_4)
+
+      expect(lottery.eligible_contestants(pick_4))to eq([alexander, grace])
+      expect(lottery.eligible_contestants(cash_5)).to eq([winston, grace])
+      expect(lottery.eligible_contestants(mega_millions)).to eq([alexander, frederick, winston, grace])
     end
   end
 end
