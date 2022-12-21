42d98f3e1a5be2ac77405955dbdf1e4ebee70303
KaraJH <karas.jones@yahoo.com>
2022-12-21 10:59:31 -0700

    feat: Contestant class with attributes and out of state, full name methods

---
+++ lib/contestant.rb
@@ -0,0 +1,21 @@
+class Contestant
+  attr_reader :first_name, :last_name, 
+              :age, :state_of_residence,
+              :spending_money, :game_interests
+  def initialize(attributes)
+    @first_name = attributes[:first_name]
+    @last_name = attributes[:last_name]
+    @age = attributes[:age]
+    @state_of_residence = attributes[:state_of_residence]
+    @spending_money = attributes[:spending_money]
+    @game_interests = []
+  end
+
+  def out_of_state? 
+    @state_of_residence != 'CO'
+  end
+
+  def full_name 
+    "#{first_name} #{last_name}"
+  end
+end
