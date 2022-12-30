a5880e7ba0eefcc697df2ba2dd8307882e043ace
KaraJH <karas.jones@yahoo.com>
2022-12-30 15:30:37 -0700

    test: Colorado lottery class with attributes

---
+++ spec/colorado_lottery_spec.rb
@@ -0,0 +1,45 @@
+require './lib/game'
+require './lib/contestant'
+require './lib/colorado_lottery'
+
+RSpec.describe ColoradoLottery do 
+  let(:lottery) {ColoradoLottery.new}
+
+  let(:pick_4) {Game.new('Pick 4', 2)}
+  let(:mega_millions) {Game.new('Mega Millions', 5, true)}
+  let(:cash_5) {Game.new('Cash 5', 1)}\
+
+  let(:alexander) {Contestant.new({first_name: 'Alexander',
+                                      last_name: 'Aigiades',
+                                      age: 28,
+                                      state_of_residence: 'CO',
+                                      spending_money: 10})}
+  let(:benjamin) {Contestant.new({
+                       first_name: 'Benjamin',
+                       last_name: 'Franklin',
+                       age: 17,
+                       state_of_residence: 'PA',
+                       spending_money: 100})}
+  let(:frederick) {Contestant.new({
+                       first_name:  'Frederick',
+                       last_name: 'Douglass',
+                       age: 55,
+                       state_of_residence: 'NY',
+                       spending_money: 20})}
+  let(:winston) {Contestant.new({
+                     first_name: 'Winston',
+                     last_name: 'Churchill',
+                     age: 18,
+                     state_of_residence: 'CO',
+                     spending_money: 5})}   
+  describe '#initialize' do 
+    it 'exists and has attributes' do 
+      expect(colorado_lottery).to be_a(ColoradoLottery)
+      expect(colorado_lottery.registered_contestants).to eq({})
+      expect(colorado_lottery.winners).to eq([])
+      expect(colorado_lottery.current_contestants).to eq({})
+    end
+  end
+
+
+end
