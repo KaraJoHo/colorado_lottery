954e7463b16c00acd6f4d598a5ff597d3d928d37
KaraJH <karas.jones@yahoo.com>
2022-12-30 15:09:26 -0700

    Game class with attributes

---
+++ spec/game_spec.rb
@@ -0,0 +1,16 @@
+require './lib/game'
+
+RSpec.describe Game do 
+  let(:pick_4) {Game.new('Pick 4', 2)}
+  let(:mega_millions) {Game.new('Mega Millions', 5, true)}
+
+  describe '#initialize' do 
+    it 'exists and has attributes' do 
+      expect(mega_millions).to be_a(Game)
+      expect(pick_4.name).to eq('Pick 4')
+      expect(pick_4.cost).to eq(5)
+      expect(pick_4.national_drawing?).to eq(false)
+      expect(mega_millions.national_drawing?).to eq(true)
+    end
+  end
+end
