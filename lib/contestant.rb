6df48a043882f5046f185d622f46e0fd3e3d6990
KaraJH <karas.jones@yahoo.com>
2022-12-30 15:21:22 -0700

    feat: Contestant class with attributes

---
+++ lib/contestant.rb
@@ -0,0 +1,21 @@
+class Contestant 
+  attr_reader :first_name, :last_name, :age,
+              :state_of_residence, :spending_money,
+              :game_interests
+  def initialize(contestant_info)
+    @first_name = contestant_info[:first_name]
+    @last_name = contestant_info[:last_name]
+    @age = contestant_info[:age]
+    @state_of_residence = contestant_info[:state_of_residence]
+    @spending_money = contestant_info[:spending_money]
+    @game_interests = []
+  end
+
+  def full_name 
+    @first_name << " " + @last_name
+  end
+
+  def out_of_state? 
+    @state_of_residence != 'CO'
+  end
+end
