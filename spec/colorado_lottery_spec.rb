e21ff1a84c0e15576685fd944597dd93c8329cc6
KaraJH <karas.jones@yahoo.com>
2022-12-30 16:16:45 -0700

    test: Eligible contestants method

--- spec/colorado_lottery_spec.rb
+++ spec/colorado_lottery_spec.rb
@@ -31,7 +31,13 @@ RSpec.describe ColoradoLottery do
                      last_name: 'Churchill',
                      age: 18,
                      state_of_residence: 'CO',
-                     spending_money: 5})}   
+                     spending_money: 5})}  
+  let(:grace) {Contestant.new({
+                     first_name: 'Grace',
+                     last_name: 'Hopper',
+                     age: 20,
+                     state_of_residence: 'CO',
+                     spending_money: 20})}                   
   describe '#initialize' do 
     it 'exists and has attributes' do 
       expect(lottery).to be_a(ColoradoLottery)
@@ -96,6 +102,64 @@ RSpec.describe ColoradoLottery do
                     "Cash 5" => [winston]
       }
       expect(lottery.registered_contestants).to eq(expected)
+
+      grace.add_game_interest('Mega Millions')
+      grace.add_game_interest('Cash 5')
+      grace.add_game_interest('Pick 4')
+      lottery.register_contestant(grace, mega_millions)
+      lottery.register_contestant(grace, cash_5)
+      lottery.register_contestant(grace, pick_4)
+
+      expected2 = {
+                    "Pick 4" => [alexander, grace],
+                    "Mega Millions" => [alexander, frederick, winston, grace],
+                    "Cash 5" => [winston, grace]
+      }
+
+      expect(lottery.registered_contestants).to eq(expected2)
+    end
+  end
+
+  describe '#eligible_contestants' do 
+    it 'returns a list of contestants who are registered and have enough money' do 
+     alexander.add_game_interest('Pick 4')
+      alexander.add_game_interest('Mega Millions')
+
+      frederick.add_game_interest('Mega Millions')
+
+      winston.add_game_interest('Cash 5')
+      winston.add_game_interest('Mega Millions')
+
+      benjamin.add_game_interest('Mega Millions')
+
+      lottery.register_contestant(alexander, pick_4) 
+      lottery.register_contestant(alexander, mega_millions) 
+      lottery.register_contestant(frederick, mega_millions)
+      lottery.register_contestant(winston, cash_5)
+      lottery.register_contestant(winston, mega_millions)
+
+      expected = {
+                    "Pick 4" => [alexander],
+                    "Mega Millions" => [alexander, frederick, winston],
+                    "Cash 5" => [winston]
+      }
+
+      grace.add_game_interest('Mega Millions')
+      grace.add_game_interest('Cash 5')
+      grace.add_game_interest('Pick 4')
+      lottery.register_contestant(grace, mega_millions)
+      lottery.register_contestant(grace, cash_5)
+      lottery.register_contestant(grace, pick_4)
+
+      expected2 = {
+                    "Pick 4" => [alexander, grace],
+                    "Mega Millions" => [alexander, frederick, winston, grace],
+                    "Cash 5" => [winston, grace]
+      }
+
+      expect(lottery.eligible_contestants(pick4)).to eq([alexander, grace])
+      expect(lottery.eligible_contestants(cash_5)).to eq(winston, grace)
+      expect(lottery.eligible_contestants(mega_millions)).to eq([alexander, frederick, winston, grace])
     end
   end
 
